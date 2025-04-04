122bf443b3ba43a2c2e62e9a021e71f4c7ccbae8
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-04-04 15:51:52 -0300

    Fix gas and parsing

--- app/src/utils/passkeyAccounts.ts
+++ app/src/utils/passkeyAccounts.ts
@@ -1,44 +1,64 @@
-import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
-import { type PublicKey, createCredential, parsePublicKey, sign, serializePublicKey } from 'webauthn-p256'
-import { type Client, queryClient } from '../config/Web3Provider';
-import { bytesToHex, encodePacked, Hex, hexToBytes, keccak256, parseSignature, stringToHex } from 'viem';
-import { signAuthorization } from 'viem/experimental';
-import { readContract, waitForTransactionReceipt, writeContract } from 'viem/actions';
-import Delegation from '../../../contracts/out/Delegation.sol/Delegation.json';
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { createCredential, parsePublicKey, sign } from "webauthn-p256";
+import { type Client } from "../config/Web3Provider";
+import {
+  bytesToHex,
+  encodePacked,
+  hexToBytes,
+  keccak256,
+  parseSignature,
+} from "viem";
+import { signAuthorization } from "viem/experimental";
+import {
+  readContract,
+  waitForTransactionReceipt,
+  writeContract,
+} from "viem/actions";
+import Delegation from "../../../contracts/out/Delegation.sol/Delegation.json";
 
-export const signUp = async ({ client, username }: { client: Client, username: string }) => {
-  const account = privateKeyToAccount(generatePrivateKey())
-  const sender = privateKeyToAccount(import.meta.env.VITE_ETHREX_RICH_WALLET_PK)
-  const initial_nonce = 0n
+export const signUp = async ({
+  client,
+  username,
+}: {
+  client: Client;
+  username: string;
+}) => {
+  const privateKey = generatePrivateKey();
+  console.log("Private key", privateKey);
+  const account = privateKeyToAccount(privateKey);
+  const sender = privateKeyToAccount(
+    import.meta.env.VITE_ETHREX_RICH_WALLET_PK,
+  );
+  const initial_nonce = 0n;
 
   const credential = await createCredential({
     user: {
       name: `Ethrex ${username}`,
       id: hexToBytes(account.address),
     },
-  })
+  });
 
-  const publicKey = parsePublicKey(credential.publicKey)
+  const publicKey = parsePublicKey(credential.publicKey);
 
   const digest = keccak256(
     encodePacked(
-      ['uint256', 'uint256', 'uint256'],
+      ["uint256", "uint256", "uint256"],
       [initial_nonce, publicKey.x, publicKey.y],
     ),
-  )
+  );
 
-  const signature = parseSignature(await account.sign({ hash: digest }))
+  const signature = parseSignature(await account.sign({ hash: digest }));
 
   const authorization = await signAuthorization(client, {
     account,
     contractAddress: import.meta.env.VITE_DELEGATION_CONTRACT_ADDRESS,
     delegate: true,
-  })
+  });
 
   const hash = await writeContract(client, {
     abi: Delegation.abi,
     address: account.address,
-    functionName: 'authorize',
+    functionName: "authorize",
     args: [
       {
         x: publicKey.x,
@@ -51,45 +71,39 @@ export const signUp = async ({ client, username }: { client: Client, username: s
       },
     ],
     authorizationList: [authorization],
-    account: sender, // defer to sequencer to fill
-  })
+    account: sender,
+    gas: 124000n, // This is a temporary fix while gas estimate is fixed in ethrex
+  });
 
-  const receipt = await waitForTransactionReceipt(client, { hash })
+  const receipt = await waitForTransactionReceipt(client, { hash });
 
   return {
     address: account.address,
     credential,
-    receipt
-  }
-}
-
+    receipt,
+  };
+};
 
 export const login = async ({ client }: { client: Client }) => {
-
   const { raw } = await sign({
-    hash: '0x',
-  })
+    hash: "0x",
+  });
 
-  const response = raw.response as AuthenticatorAssertionResponse
-  const address = bytesToHex(new Uint8Array(response.userHandle!))
+  const response = raw.response as AuthenticatorAssertionResponse;
+  const address = bytesToHex(new Uint8Array(response.userHandle!));
 
-  console.log(address)
-
-  const [publicKey] = await readContract(client, {
+  const [publicKeyX, publicKeyY] = (await readContract(client, {
     address,
     abi: Delegation.abi,
-    functionName: 'authorizedKey',
-  }) as [PublicKey]
-
-  console.log('publicKey', publicKey)
-  console.log('credential', raw)
+    functionName: "authorizedKey",
+  })) as [bigint, bigint];
 
   return {
     address,
     credential: {
       id: raw.id,
-      publicKey: serializePublicKey(publicKey),
-      raw
-    }
-  }
-}
+      publicKey: `0x${publicKeyX.toString(16)}${publicKeyY.toString(16)}`,
+      raw,
+    },
+  };
+};
